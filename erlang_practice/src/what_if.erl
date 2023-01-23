faccc78d8738fbd4d0ee33751b432080d13704f3
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-01-24 01:10:53 +0300

    syntax-func added

--- erlang_practice/src/what_if.erl
+++ erlang_practice/src/what_if.erl
@@ -1,10 +1,9 @@
 -module(what_if).
-
 -export([it_is_fine/0]).
-
 -export([oh_god/1]).
-
 -export([animal_sound/1]).
+-export([insert/2]).
+-export([sun_temp/1]).
 
 %% As such, when Erlang can't find a way to have a guard succeed,
 %% it will crash: it cannot not return something. "14.row.."
@@ -34,4 +33,26 @@ animal_sound(Animal)->
            true-> "asdaseqwe"
          end,
   {Animal,"says" ++ Talk ++ "!"}.
+
+%% Insert to list.
+insert(X,[])->
+  [X];
+insert(X,Set)->
+  case lists:member(X,Set) of
+    true -> Set;
+    false -> [X|Set]
+  end.
+
+%% Temperature check.
+sun_temp(Temperature) ->
+  case Temperature of
+    {celsius, N} when N >= 20, N =< 45 ->
+      'favorable';
+    {kelvin, N} when N >= 293, N =< 318 ->
+      'scientifically favorable';
+    {fahrenheit, N} when N >= 68, N =< 113 ->
+      'favorable in the US';
+    _ ->
+      'avoid beach'
+  end.
 
